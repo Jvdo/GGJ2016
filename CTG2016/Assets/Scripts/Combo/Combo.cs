1456005cb3d1a9308d65f1cc5a338b5c9ae8c67d
Jeroen Matser <mail@jmatser.com>
2016-01-29 21:30:50 +0100

    Initial work on combos.

---
+++ CTG2016/Assets/Scripts/Combo/Combo.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Combo {
+
+	public enum Entry
+	{
+		Up,
+		Down,
+		Left,
+		Right,
+	}
+
+	static Entry GetRandomEntry()
+	{
+		Array values = Enum.GetValues(typeof(Entry));
+		return (Entry)values.GetValue(UnityEngine.Random.Range(0, values.Length));
+	}
+
+	List<Entry> entries = new List<Entry>();
+
+	public Combo(int size = 4)
+	{
+		Generate(size);
+	}
+
+	public void Generate(int size)
+	{
+		const int threshold = 2;
+
+		entries.Clear();
+
+		for (int i = 0; i < size; ++i)
+		{
+			int existingCount = 0;
+			do
+			{
+				Entry entry = GetRandomEntry();
+				existingCount = 0;
+
+				for (int j = 0; j < entries.Count; ++j)
+				{
+					if (entries[j] == entry)
+					{
+						++existingCount;
+					}
+				}
+				if (existingCount <= threshold)
+				{
+					entries.Add(entry);
+				}
+			} while (existingCount > threshold);
+		}
+	}
+
+	public bool Equals(Combo c)
+	{
+		return entries == c.entries;
+	}
+
+	public void Print()
+	{
+		String msg = "Combo: ";
+		foreach(Entry e in entries)
+		{
+			msg += e.ToString() + ", ";
+		}
+
+		Debug.Log(msg);
+	}
+}
---
+++ CTG2016/Assets/Scripts/Combo/ComboDatabase.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ComboDatabase : MonoBehaviour {
+
+	List<Combo> combos = new List<Combo>();
+
+	public int comboLength = 4;
+
+	// Use this for initialization
+	void Start () {
+
+		// Debug code...
+		InitializeCombos(5);
+		Print();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitializeCombos(int numCombos)
+	{
+		for (int i = 0; i < numCombos; ++i)
+		{
+			Combo c = new Combo(comboLength);
+			while (ContainsCombo(c))
+			{
+				c.Generate(comboLength);
+			}
+			combos.Add(c);
+		}
+	}
+
+	public bool ContainsCombo(Combo c)
+	{
+		foreach (var combo in combos)
+		{
+			if (combo.Equals(c))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void Print()
+	{
+		foreach(Combo c in combos)
+		{
+			c.Print();
+		}
+	}
+}
