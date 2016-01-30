bc588ee4ddafd6475a16c42324c3cb97cd00f0e8
Remco Hilbert <remco_hilbert@hotmail.com>
2016-01-30 19:00:23 +0100

    Health Bar Operational

--- CTG2016/Assets/Scripts/PlayerBehaviour.cs
+++ CTG2016/Assets/Scripts/PlayerBehaviour.cs
@@ -13,11 +13,14 @@ public class PlayerBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 		health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		UpdateHealthBar ();
 
 		if (Input.GetKeyDown (KeyCode.H)) {
 			
@@ -33,5 +36,33 @@ public class PlayerBehaviour : MonoBehaviour {
 		{
 			print("TODO: game over");
 		}
+	}
+
+	public float factor = 0.5f;
+
+	public Transform fgTransform;
+	public MeshRenderer fgRenderer;
+	Material fgMaterial;
+
+
+
+	// Update is called once per frame
+	void UpdateHealthBar () {
+
+		factor = health / 100f;
+		Vector3 localScale = fgTransform.localScale;
+		localScale.x = factor;
+		fgTransform.localScale = localScale;
+
+
+		if (fgRenderer != null)
+		{
+			fgRenderer.material.mainTextureScale = new Vector2(factor, 1.0f);
+		}
+	}
+
+	public void SetFactor(float f)
+	{
+		factor = Mathf.Clamp01(f);
 	}
 }
