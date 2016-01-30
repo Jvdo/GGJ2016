fbf628ad5afa66904c78d6fb0bd4ad9d87f0434e
Jeroen Matser <mail@jmatser.com>
2016-01-30 12:21:20 +0100

    Scores/Difficulty WIP.

---
+++ CTG2016/Assets/Scripts/Scores/Difficulty.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Difficulty : MonoBehaviour {
+
+	int[] difficultyLevels = new int[3];
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < difficultyLevels.Length; ++i)
+		{
+			difficultyLevels[i] = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnEnemyDestroyed(Enemy enemy)
+	{
+		difficultyLevels[enemy.id]++;
+	}
+
+	public void OnDamageTake(Enemy enemy)
+	{
+		difficultyLevels[enemy.id] = 0;
+	}
+			
+}
