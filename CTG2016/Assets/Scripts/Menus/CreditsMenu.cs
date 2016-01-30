091e2a9ccda384a426f3a5bb54ae211e9265886c
Jeroen Matser <mail@jmatser.com>
2016-01-30 09:05:42 +0100

    Added credits menu placeholder.

---
+++ CTG2016/Assets/Scripts/Menus/CreditsMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown)
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+		}
+	}
+}
